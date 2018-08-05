b6a8642a5841f02ebb3c55327de217a922333ab6
alfieqashwa <alfieqashwa@gmail.com>
2018-08-06 06:35:29 +0700

    Props example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
-class Cello extends Component {
-  render() {
-    var kanban = "Kanban";
-    return (
-      <div><h1>Cello { kanban } </h1></div>
-    );
-  }
+// Parent Component
+class GroceryList extends Component {
+    render() {
+        return (
+            <ul>
+                <ListItem quantity="1">Bread</ListItem>
+                <ListItem quantity="6">Eggs</ListItem>
+                <ListItem quantity="2">Milk</ListItem>
+            </ul>
+        );
+    }
 }
 
-export default Cello;
+// Child Component
+class ListItem extends Component {
+    render() {
+        return(
+            <li>
+                {this.props.quantity} x {this.props.children}
+            </li>
+        );
+    }
+}
+
+export default GroceryList;
