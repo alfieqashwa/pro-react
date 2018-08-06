7ddebcc84f9e73f17b2a802b5e76a6e134774d72
alfieqashwa <alfieqashwa@gmail.com>
2018-08-06 07:17:20 +0700

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import KanbanBoard from './KanbanBoard';
 
-// Parent Component
-class GroceryList extends Component {
-    render() {
-        return (
-            <ul>
-                <ListItem quantity="1">Bread</ListItem>
-                <ListItem quantity="6">Eggs</ListItem>
-                <ListItem quantity="2">Milk</ListItem>
-            </ul>
-        );
-    }
-}
+let cardList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        description: "I should read the whole book",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own Expeeriments",
+                done: false
+            }
+        ]
+    },
+];
 
-// Child Component
-class ListItem extends Component {
-    render() {
-        return(
-            <li>
-                {this.props.quantity} x {this.props.children}
-            </li>
-        );
-    }
-}
-
-export default GroceryList;
+export default KanbanBoard;
