804e7541aed405c8826013821901fbede54b02b5
alfieqashwa <alfieqahwa@gmail.com>
2018-08-25 16:34:49 +0700

    Fixed the App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import KanbanBoard from "./KanbanBoard";
+
+let cardList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own Expeeriments",
+        done: false
+      }
+    ]
+  }
+];
+
+ReactDOM.render(
+  <KanbanBoard cards={cardList} />,
+  document.getElementById("root")
+);
 registerServiceWorker();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
