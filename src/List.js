e79b8d3db505caa144cfd44991335a6f69993b3c
alfieqashwa <alfieqahwa@gmail.com>
2018-08-26 02:03:12 +0700

    Finished and Fixed Chapter2

--- src/List.js
+++ src/List.js
@@ -3,12 +3,14 @@ import Card from "./Card";
 
 class List extends Component {
   render() {
-    const cards = this.props.cards.map(card => {
+    let cards = this.props.cards.map(card => {
       return (
         <Card
+          key={card.id}
           id={card.id}
           title={card.title}
           description={card.description}
+          color={card.color}
           tasks={card.tasks}
         />
       );
